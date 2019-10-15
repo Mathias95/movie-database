73405a78f2a22a1adbcd619ece15ea3d8b3ce3fe
Mathias Jacobsen <mathiasrjacobsen@gmail.com>
2019-10-15 23:05:50 +0200

    fixed the index banner

--- pages/index.vue
+++ pages/index.vue
@@ -2,14 +2,16 @@
 	<section class="welcome">
 		<div class="welcome__background"></div>
 		<div class="container welcome__wrapper">
-			<div class="row">
-				<div class="col-12 col-sm-10 col-md-7 col-lg-6 mr-auto">
-					<h1 class="welcome__title">Your Movie &amp; TV Show Library</h1>
-					<p class="welcome__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-					<nuxt-link to="/home" class="btn btn-secondary">Discover now<i class="material-icons top__icon">keyboard_arrow_right</i></nuxt-link>
+			<div class="row welcome__wrapper-inner">
+				<div class="col-12 col-sm-10 col-md-7 col-lg-6 mr-auto welcome__left">
+					<div>
+						<h1 class="welcome__title">Your Movie &amp; TV Show Library</h1>
+						<p class="welcome__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+						<nuxt-link to="/home" class="btn btn-secondary">Discover now<i class="material-icons top__icon">keyboard_arrow_right</i></nuxt-link>
+					</div>
 				</div>
-				<div class="col-12 col-lg-6">
-					<img src="" alt="" class="welcome__img">
+				<div class="col-12 col-lg-5 welcome__right">
+					<img src="~assets/images/mobile-index.png" alt="" class="welcome__img">
 				</div>
 			</div>
 		</div>
