8dd4613cc2dbf0224ae287760a68cb30022bf974
Mathias Jacobsen <mathiasrjacobsen@gmail.com>
2019-10-15 20:22:31 +0200

    init + main setup files

---
+++ components/Logo.vue
@@ -0,0 +1,10 @@
+<template>
+	<div class="logo">
+		<div class="logo__outer">
+			<div class="logo__inner">
+				<div class="logo__icon"></div>
+			</div>
+		</div>
+		<p class="logo__text">Movie Database</p>
+	</div>
+</template>
---
+++ layouts/welcomelayout.vue
@@ -0,0 +1,24 @@
+<template>
+		<div>
+			<header class="top-header">
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col">
+							<logo/>
+						</div>
+					</div>
+				</div>
+			</header>
+			<nuxt/>
+		</div>
+</template>
+
+<script>
+import Logo from "~/components/Logo.vue";
+
+export default {
+	components: {
+		Logo
+	}
+}
+</script>
