4c85619b49fee58854c0e06c2069cce72e290da1
Mathias Jacobsen <mathiasrjacobsen@gmail.com>
2019-10-18 16:50:48 +0200

    added responsive navigation

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,13 +1,48 @@
 <template>
-	<TopHeader/>
+	<div>
+		<header class="header">
+			<div class="header__mobile-wrapper">
+				<button @click="toggle" class="header__mobile" v-bind:class="{ 'is-active': offCanvas }">
+					<span class="header__mobile-line header__mobile-line--1"></span>
+					<span class="header__mobile-line header__mobile-line--2"></span>
+					<span class="header__mobile-line header__mobile-line--3"></span>
+				</button>
+				<logo class="header__logo"/>
+				<div class="header__search">
+					<Search/>
+				</div>
+			</div>
+		</header>
+		
+		<aside class="sidebar trans-sidebar" v-bind:class="{'on': offCanvas}">
+			<nav class="sidebar__menu">
+				<ul class="sidebar__list">
+					<li class="sidebar__item"><nuxt-link to="/" class="sidebar__link">Trending movies</nuxt-link></li>
+				</ul>
+			</nav>
+		</aside>
+		<div @click="toggle" class="sidebar__overlay" v-bind:class="{'on': offCanvas }"></div>
+	</div>
 </template>
 
 <script>
-import TopHeader from "~/components/TopHeader.vue";
+import Logo from "~/components/Logo.vue";
+import Search from "~/components/Search.vue";
 
 export default {
+	data(){
+		return {
+			offCanvas: false
+		}
+	},
+	methods: {
+		toggle() {
+			this.offCanvas = !this.offCanvas;
+		}
+	},
 	components: {
-		TopHeader
+		Logo,
+		Search
 	}
 }
 </script>
